a9bf35acfef17c510139ff02b8cd7abd36967f6d
Sarmigan Sritharan <sarmigan@live.co.uk>
2024-01-28 04:01:55 +0000

    tidy up redirection links

--- client/app/page.js
+++ client/app/page.js
@@ -14,7 +14,7 @@ export default function Page() {
  
   return (
     <form onSubmit={onSubmit} className="h-screen flex flex-col items-center justify-center">
-      <input type="text" required="true" name="artist" className="focus:outline-none focus:scale-125 duration-300 rounded-lg p-4 m-5 md:uppercase text-center"/>
+      <input type="text" required={true} name="artist" className="focus:outline-none focus:scale-125 duration-300 rounded-lg p-4 m-5 md:uppercase text-center"/>
       <br></br>
       <button type="submit" className="shadow-[0_9px_0_rgb(0,0,0)] hover:shadow-[0_4px_0px_rgb(0,0,0)] ease-out hover:translate-y-1 transition-all font-bold p-2 border-solid border-2 rounded-lg border-black">FIND SIMILAR ARTISTS!</button>
     </form>
